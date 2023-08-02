6dd657635fbda7215d1e06b072d69c6dff07b9f0
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-08-01 20:26:13 -0600

    feat(frontend): the categories were added and it already shows the gifs and their information

--- src/GifSearchEngine.jsx
+++ src/GifSearchEngine.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifSearchEngine = () => {
 
-	const [categories, setCategories] = useState([ 'One punch', 'Attack On Titan', 'Hunter X Hunter' ]);
+	const [categories, setCategories] = useState([ 'One punch']);
 	
 	const onAddCategorie = (newCategory) => {
 		
@@ -14,23 +15,22 @@ export const GifSearchEngine = () => {
 
 	return (
 		<>
-			{/* titulo */}
+
 			<h1>Gif Search Engine</h1>
 
-			{/* Input */}
+			
 			<AddCategory
 				onNewCategory={ (event) => onAddCategorie(event) }
-				// setCategories={ setCategories }
 			/>
-			{/* Listado de Gif */}
-				{/* Gif Item */}
-				<ol>
-					{
-						categories.map( category => {
-							return <li key={ category }>{ category }</li>
-						})
-					}
-				</ol>
+				
+				{
+					categories.map( (category) => (
+						<GifGrid
+							key={ category }	
+							category={ category }
+						/>
+					))
+				}
 		</>
 	);
 }
